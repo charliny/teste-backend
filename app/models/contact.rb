926ed57965b3042e54805d721f3e87ac5f5581e1
charliny <jessicacharliny@gmail.com>
2022-05-10 13:30:48 -0300

    validates :cpf

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,20 @@
 class Contact < ApplicationRecord
-    validates :name,  length: {minimum:4}
-    validates :active, :birthday, :email, :mobile, :advertising, presence: true
+    validates :name,  
+    length: {
+        minimum: 5, 
+        maximum: 10
+    }
+
+    validates :active, :birthday, :email, :mobile, :advertising, :cpf, presence: true
+    
+    validates :cpf, 
+    length: { 
+        in: 10..14 
+    }
+    
+    validates :cpf, 
+    format: { 
+        with: /\A\d+\z/, 
+        message: "only numbers" 
+    }
 end
